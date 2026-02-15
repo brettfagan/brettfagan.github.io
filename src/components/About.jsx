cd2378c2b19e2cf52433c7c20f546bfbfa0bfe14
faganbrett-lab <fagan.brett@gmail.com>
2026-02-14 22:23:45 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,10 @@ function About() {
       <div className="container">
         <h2>About</h2>
         <p>
-          I&apos;m a developer focused on creating clean, user-friendly web
-          experiences. My interests include front-end engineering, data-driven
-          utilities, and building products that make complex tasks easier to
-          understand.
+          I&apos;m a Product Manager and developer focused on creating clean,
+          user-friendly web experiences. My interests include front-end
+          engineering, data-driven utilities, and building products that make
+          complex tasks easier to understand.
         </p>
       </div>
     </section>
